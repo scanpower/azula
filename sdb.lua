ca3371321e95a05d4fc2a96aa8e5008c0474005f
Zak McCreery <zakm@incirrus.com>
2012-11-02 23:40:44 -0400

    Added xml parser to make sane the select response

--- sdb.lua
+++ sdb.lua
@@ -1,4 +1,5 @@
 require 'scanpower/azula/util.lua'
+require 'scanpower/azula/xmlp.lua'
 
 local default = {
     region = 'us-east-1',
@@ -26,6 +27,43 @@ function proto:url(param)
                       nil, param)
 end
 
+local function parseSelectResponse(xmlstr)
+    local xml = xmlp.parse(xmlstr)
+    local rows = {}
+    if #xml > 0 and xml[1].children then
+        for _,node in ipairs(xml[1].children) do
+            if type(node) == 'table' and node.name == 'SelectResponse' then
+                for _,response in ipairs(node.children) do
+                    if response.name == 'SelectResult' then
+                        for _,item in ipairs(response.children) do
+                            if item.name == 'Item' then
+                                local row = {}
+                                for _,attr in ipairs(item.children) do
+                                    if attr.name == 'Name' then
+                                        log('itemName()')
+                                        row['itemName()'] = table.concat(attr.children,'')
+                                    elseif attr.name == 'Attribute' then
+                                        local cname, cval
+                                        for _,n in ipairs(attr.children) do
+                                            if n.name == 'Name' then cname = table.concat(n.children,'') end
+                                            if n.name == 'Value' then cval = table.concat(n.children,'') end
+                                        end
+                                        if cname and cval then
+                                            row[cname] = cval
+                                        end
+                                    end -- attr.name
+                                end -- item.children
+                                rows[#rows+1] = row
+                            end -- Item
+                        end -- response.children
+                    end -- SelectResult
+                end -- node.children
+            end -- SelectResponse
+        end -- xml.children
+    end
+    return rows
+end
+
 function proto:select(SelectExpression,ConsistentRead,NextToken)
     if not SelectExpression:lower():find '^%s*select' then
         SelectExpression = 'select ' .. SelectExpression
@@ -37,9 +75,9 @@ function proto:select(SelectExpression,ConsistentRead,NextToken)
         ConsistentRead = ConsistentRead,
         NextToken = NextToken
     }
-    return http.request({
+    return parseSelectResponse( http.request({
         url = url
-    }).content
+    }).content )
 end
 
 return sdb
